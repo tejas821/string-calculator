1719ced1f47db4eca32c8ed82121966327b553e1
Tejas Kadam <tejasdkadam821@gmail.com>
2024-02-17 14:59:47 +0530

    test cases added for single number and sum of two number

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,8 +5,37 @@ var StringCalculator = /** @class */ (function () {
     function StringCalculator() {
     }
     StringCalculator.prototype.add = function (numbers) {
-        // Implementation goes here
-        return 0;
+        if (numbers === "") {
+            return 0;
+        }
+        // Default delimiter is comma and newline
+        var delimiterRegex = /,|\n/;
+        var delimiter = delimiterRegex.source;
+        // Check if custom delimiter is provided
+        if (numbers.startsWith("//")) {
+            var customDelimiterMatch = numbers.match(/^\/\/(.*)\n/);
+            if (customDelimiterMatch) {
+                delimiter = customDelimiterMatch[1];
+                numbers = numbers.substring(customDelimiterMatch[0].length);
+            }
+        }
+        var numberArray = numbers.split(new RegExp(delimiter));
+        var sum = 0;
+        var negatives = [];
+        for (var _i = 0, numberArray_1 = numberArray; _i < numberArray_1.length; _i++) {
+            var numStr = numberArray_1[_i];
+            var num = parseInt(numStr);
+            if (num < 0) {
+                negatives.push(num);
+            }
+            if (num < 1000) {
+                sum += num;
+            }
+        }
+        if (negatives.length > 0) {
+            throw new Error("negative numbers not allowed ".concat(negatives.join(",")));
+        }
+        return sum;
     };
     return StringCalculator;
 }());
--- StringCalculator.test.ts
+++ StringCalculator.test.ts
@@ -1,7 +1,7 @@
 import { StringCalculator } from './StringCalculator';
 
 describe("StringCalculator", () => {
-    let stringCalculator:StringCalculator;
+    let stringCalculator: StringCalculator;
 
     beforeEach(() => {
         stringCalculator = new StringCalculator();
@@ -9,6 +9,13 @@ describe("StringCalculator", () => {
 
     it("should return 0 for an empty string", () => {
         expect(stringCalculator.add("")).toBe(0);
+    });
+    it("should return the number itself for a single number", () => {
+        expect(stringCalculator.add("1")).toBe(1);
+    });
+
+    it("should return the sum of two numbers", () => {
+        expect(stringCalculator.add("1,5")).toBe(6);
     });
 
 });
