785ca5419946e9e263ed694336f13e6cf8089597
Tejas Kadam <tejasdkadam821@gmail.com>
2024-02-17 14:53:56 +0530

    added regx for default delimiter is comma and newline, also same for custome delimiter based on it splited the string

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -3,7 +3,20 @@ export class StringCalculator {
         if (numbers === "") {
             return 0;
         }
+        // Default delimiter is comma and newline
+        const delimiterRegex = /,|\n/;
+        let delimiter = delimiterRegex.source;
 
+        // Check if custom delimiter is provided
+        if (numbers.startsWith("//")) {
+            const customDelimiterMatch = numbers.match(/^\/\/(.*)\n/);
+            if (customDelimiterMatch) {
+                delimiter = customDelimiterMatch[1];
+                numbers = numbers.substring(customDelimiterMatch[0].length);
+            }
+        }
+
+        const numberArray = numbers.split(new RegExp(delimiter));
         
     }
 }
